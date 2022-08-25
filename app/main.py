6ddfce9083255c186d4a514626a9fbff83b1b52a
Harishankar Kumar <hari01584@gmail.com>
2022-08-25 16:25:27 +0530

    cors enable

--- app/main.py
+++ app/main.py
@@ -2,7 +2,19 @@ from typing import Union
 
 from fastapi import FastAPI
 from bhuvan import BhuvanScraper
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
